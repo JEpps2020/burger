f07cd0532851d490fbb2bc9321577e2602652e77
Jasmine Epps <epps.jasmine@gmail.com>
2020-04-30 19:51:03 -0400

    I have created the starter files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Bison Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Elk Burger");
