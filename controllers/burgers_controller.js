20c3bcfcab1b82851c6754a849d7ad6b6cb4b800
Jasmine Epps <epps.jasmine@gmail.com>
2020-05-02 20:53:50 -0400

    I have updated my index.handlebars,burger controllers.js,css.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.createOne([
+  burger.insertOne([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.updateOne({
-    devoured: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ("Bison Burger");
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Elk Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Elk Burger", 0);
